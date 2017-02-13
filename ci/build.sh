5e52d98c8af7bd090a67ea7db036454d15269beb
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-13 12:47:59 -0600

    Add additional tests

--- ci/build.sh
+++ ci/build.sh
@@ -20,7 +20,10 @@ cargo test --verbose
 # Smoke test the result.
 export RUST_LOG=debug
 cargo run http://example.com/
-./target/debug/hyperf http://example.com/
 ./target/debug/hyperf --help
 ./target/debug/hyperf --version
+./target/debug/hyperf http://example.com/
 ./target/debug/hyperf --verbose http://example.com/
+./target/debug/hyperf --requests 10 http://example.com/
+./target/debug/hyperf -n 10 http://example.com/
+./target/debug/hyperf -n 10 --concurrency 10 http://example.com/
