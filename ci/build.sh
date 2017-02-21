05497d9898ebe0372165d5ca8b4c49b815886ec4
Dolph Mathews <dolph.mathews@gmail.com>
2017-02-21 05:00:46 -0600

    Update smoke tests to include http method

--- ci/build.sh
+++ ci/build.sh
@@ -23,9 +23,9 @@ export RUST_LOG=debug
 cargo run http://example.com/
 ./target/debug/hyperf --help
 ./target/debug/hyperf --version
-./target/debug/hyperf http://example.com/
-./target/debug/hyperf --verbose http://example.com/
-./target/debug/hyperf --requests 10 http://example.com/
-./target/debug/hyperf -n 10 http://example.com/
-./target/debug/hyperf -n 10 --concurrency 10 http://example.com/
-./target/debug/hyperf -n 10 -c 10 http://example.com/
+./target/debug/hyperf get http://example.com/
+./target/debug/hyperf --verbose get http://example.com/
+./target/debug/hyperf --requests 10 get http://example.com/
+./target/debug/hyperf -n 10 get http://example.com/
+./target/debug/hyperf -n 10 --concurrency 10 get http://example.com/
+./target/debug/hyperf -n 10 -c 10 get http://example.com/
